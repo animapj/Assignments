7ec95917040a17bb97b549e09677a17d97a8ade5
animapj <animeshjoshi1998@gmail.com>
2021-05-07 14:44:40 +0530

    assignment 4 final

--- Assignment_4/script.js
+++ Assignment_4/script.js
@@ -1,6 +1,6 @@
 
 (function() {
-    var names = ["Yamini", "Jacob", "Jenny", "Jayant", "Priya", "Winston", "Tim", "Jagan", "Lokesh", "Jeremy"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
